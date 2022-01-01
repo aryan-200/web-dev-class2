c897d83bced66ce239fabf8121f388de455bc385
aryan <50gamer.aamg@gmail.com>
2022-01-01 16:04:10 +0330

    week9

--- week9/project/js/script.js
+++ week9/project/js/script.js
@@ -1,5 +1,6 @@
 const url = 'https://randomuser.me/api';
 const refresh = document.getElementById('refresh')
+const profile = document.getElementById('profile')
 const img = document.getElementById('image');
 const email = document.getElementById('email');
 const phone = document.getElementById('phon');
@@ -22,8 +23,23 @@ fetch(url)
     phone.innerHTML = data.results[0].phone;
     age.innerHTML = data.results[0].registered.age;
     address.innerHTML = data.results[0].location.street.name;
+    profile.innerHTML = data.results[0].name.title;
 })
 
-refresh.addEventListener('click',  function refresh(){
-    window.location.reload("Refresh")
+refresh.addEventListener('click',  function (data){
+    fetch(url)
+    .then(function (response) {
+    return response.json()
+
+    })
+    .then(function (data) {
+        console.log(data)
+        img.src = data.results[0].picture.large;
+        nam.innerHTML = data.results[0].name.first + data.results[0].name.last;
+        locationl.innerHTML = data.results[0].location.city +',' + data.results[0].location.country;
+        email.innerHTML = data.results[0].email;
+        phone.innerHTML = data.results[0].phone;
+        age.innerHTML = data.results[0].registered.age;
+        address.innerHTML = data.results[0].location.street.name;
+    })
 })
