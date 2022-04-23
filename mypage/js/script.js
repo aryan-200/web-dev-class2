a3016577f484126c5c63fb1f434b3ce991e352ef
aryan <50gamer.aamg@gmail.com>
2022-04-23 15:34:54 +0430

    mypage

---
+++ mypage/js/script.js
@@ -0,0 +1,13 @@
+//movecat , secret site , code generater , api , musice , cart etebary , hesab hoghogh
+//https://aryan-200.github.io/web-dev-class2/week1/project/index.html
+//web-dev-class2/week2/project/index.html
+//web-dev-class2/week3/project/index.html
+//web-dev-class2/week4/projects/index.html
+//web-dev-class2/week6/project/index.html
+//web-dev-class2/week7/project/index.html
+//web-dev-class2/week8/project/index.html
+//web-dev-class2/week9/project/index.html
+//web-dev-class2/week10/project/index.html
+//web-dev-class2/week11/index.html
+//web-dev-class2/week12/index.html
+//web-dev-class2/week13/index.html
